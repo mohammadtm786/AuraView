792175c5e845157ccd11c0627b02b0c9359be187
Mohammad Talib Mansoori <127229179+mohammadtm786@users.noreply.github.com>
2025-07-07 16:00:20 +0530

    Add files via upload

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import StreamClientProvider from "@/components/providers/StreamClientProvider";
+import React from "react";
+
+function Layout({ children }: { children: React.ReactNode }) {
+    return <StreamClientProvider>{children}</StreamClientProvider>;
+}
+export default Layout;
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://cute-trout-71.clerk.accounts.dev/",
+            applicationID: "convex",
+        },
+    ]
+};
